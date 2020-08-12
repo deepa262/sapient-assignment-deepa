1d8840bd60bf64598a138b1483a01d362d7784df
deepa262 <36667957+deepa262@users.noreply.github.com>
2020-08-12 20:49:57 +0530

    Add files via upload

---
+++ precache-manifest.d64321a28bfcc60f0b9522ec7284fd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df539b30a92866bca70ab12ee4cd9c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad86d57539ede00d7ea7",
+    "url": "/static/css/2.98439ec7.chunk.css"
+  },
+  {
+    "revision": "2c38bb3df7880177d3d8",
+    "url": "/static/css/main.ad0f349c.chunk.css"
+  },
+  {
+    "revision": "ad86d57539ede00d7ea7",
+    "url": "/static/js/2.d7d650f4.chunk.js"
+  },
+  {
+    "revision": "348e18c767030a34e3e00df7559622cb",
+    "url": "/static/js/2.d7d650f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c38bb3df7880177d3d8",
+    "url": "/static/js/main.e02646d4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
